d33c567abaf5b5393c0148b9555bf453dd99596b
Millena <contatomillenarodrigues20@gmail.com>
2023-02-17 22:11:13 -0300

    perfil

--- header.php
+++ header.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <!-- header.php -->
+<?php include_once "sessao-login.php" ?>
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
--- perfil-painel.php
+++ perfil-painel.php
@@ -1,5 +1,19 @@
 <?php 
-session_start();
+include_once "sessao-login.php";
+include_once "header.php";
+include_once "menu.php";
 ?>
+
+<main class="container">
+    <div class="menu">
+        <?php include_once "menu.php"; ?>
+    </div>
+    <!-- <a href="logout.php"> <?php echo $_SESSION["usuario"]; ?> [ Sair ]</a> -->
+    <h1>Painel</h1>
+</main>
+<?php
+include_once "footer.php";
+?>
+
 <a href="logout.php"><?php echo $_SESSION["usuario"]; ?> [ sair ] </a>
 <h1>Painel</h1>
